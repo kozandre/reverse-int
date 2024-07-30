d5aed06baddcba32ddec3be85d307c274f57ff46
Andrey Kozimirov <an.kozimirov@gmail.com>
2024-07-30 14:45:00 +0300

    feat: implement reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  const numStr = String(n);
+  let result = numStr;
+  if (numStr.startsWith('-')) {
+    result = numStr.slice(1);
+  } else if (numStr.endsWith('0')) {
+    result = numStr.slice(0, -1);
+  }
+  return result.split('').reverse().join('');
 }
